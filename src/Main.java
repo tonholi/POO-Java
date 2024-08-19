446c893d5d52eb68c77ffaf34edff7f09f618ad4
Luis Gustavo <luisgtnunes@hotmail.com>
2024-08-18 23:33:10 -0300

    Adicionado Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,31 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        Curso cursoJava = new Curso();
+        cursoJava.setTitulo("Curso Java");
+        cursoJava.setDescricao("Curso java POO");
+        cursoJava.setCargaHoraria(8);
+
+        Curso cursoJs = new Curso();
+        cursoJs.setTitulo("Curso JS");
+        cursoJs.setDescricao("Curso Javascript");
+        cursoJs.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria java");
+        mentoria.setDescricao("mentoria java descricao");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(cursoJava);
+        System.out.println(cursoJs);
+        System.out.println(mentoria);
 
     }
 }
